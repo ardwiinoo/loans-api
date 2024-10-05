f493cc7540b59e494e8082348c406421d44db558
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-05 13:05:32 +0700

    chore: init

---
+++ src/main/java/com/ardwiinoo/loansapi/LoansapiApplication.java
@@ -0,0 +1,14 @@
+package com.ardwiinoo.loansapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LoansapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ardwiinoo/loansapi/LoansapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardwiinoo.loansapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
