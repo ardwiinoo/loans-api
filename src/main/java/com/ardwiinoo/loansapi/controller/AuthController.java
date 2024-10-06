be211355a2ce078ee472909283b67ab197f87f54
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-06 12:19:59 +0700

    add auth controller, add user register

---
+++ src/main/java/com/ardwiinoo/loansapi/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.ardwiinoo.loansapi.controller;
+
+import com.ardwiinoo.loansapi.model.dto.user.UserDto;
+import com.ardwiinoo.loansapi.model.dto.user.UserRegisterRequest;
+import com.ardwiinoo.loansapi.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, Object>> userRegisterHandler(@RequestBody UserRegisterRequest request) {
+        UserDto result = authService.userRegister(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+                "status", "success",
+                "data", Map.of(
+                        "userId", result.getId()
+                )
+        ));
+    }
+}
