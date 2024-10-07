3cac919754799cb46c31ac83ae21a552ef9698b4
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-07 20:05:09 +0700

    add lombok annotation

--- src/main/java/com/ardwiinoo/loansapi/model/dto/user/UserTokenResponse.java
+++ src/main/java/com/ardwiinoo/loansapi/model/dto/user/UserTokenResponse.java
@@ -1,8 +1,10 @@
 package com.ardwiinoo.loansapi.model.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserTokenResponse {
     private String accessToken;
     private String refreshToken;
