5feea8f05aa4bdab216a0317b4ec97d57b8dfbf6
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-07 20:04:15 +0700

    add user login test scenario

--- src/test/java/com/ardwiinoo/loansapi/impl/AuthServiceImplTest.java
+++ src/test/java/com/ardwiinoo/loansapi/impl/AuthServiceImplTest.java
@@ -1,11 +1,17 @@
 package com.ardwiinoo.loansapi.impl;
 
+import com.ardwiinoo.loansapi.exception.AuthenticationError;
 import com.ardwiinoo.loansapi.exception.InvariantError;
 import com.ardwiinoo.loansapi.mapper.UserMapper;
 import com.ardwiinoo.loansapi.model.dto.user.UserDto;
+import com.ardwiinoo.loansapi.model.dto.user.UserLoginRequest;
 import com.ardwiinoo.loansapi.model.dto.user.UserRegisterRequest;
+import com.ardwiinoo.loansapi.model.dto.user.UserTokenResponse;
+import com.ardwiinoo.loansapi.model.entity.Authentication;
 import com.ardwiinoo.loansapi.model.entity.User;
+import com.ardwiinoo.loansapi.repository.AuthenticationRepository;
 import com.ardwiinoo.loansapi.repository.UserRepository;
+import com.ardwiinoo.loansapi.service.JWTService;
 import com.ardwiinoo.loansapi.service.impl.AuthServiceImpl;
 import com.ardwiinoo.loansapi.util.ValidationUtil;
 import org.junit.jupiter.api.Assertions;
@@ -25,10 +31,16 @@ public class AuthServiceImplTest {
     private UserRepository userRepository;
 
     @Mock
+    private AuthenticationRepository authenticationRepository;
+
+    @Mock
     private PasswordEncoder passwordEncoder;
 
     @Mock
     private ValidationUtil validationUtil;
+
+    @Mock
+    private JWTService jwtService;
 
     @BeforeEach
     void setup() {
@@ -114,5 +126,120 @@ public class AuthServiceImplTest {
         Assertions.assertEquals(mappedUserDto.getFirstName(), result.getFirstName());
         Assertions.assertEquals(mappedUserDto.getLastName(), result.getLastName());
         Assertions.assertEquals(mappedUserDto.isEnable(), result.isEnable());
+    }
+
+    // TEST USER LOGIN
+    @Test
+    void userLogin_ShouldThrowInvariantError_WhenValidationFails() {
+        UserLoginRequest request = new UserLoginRequest(
+                "",
+                ""
+        );
+
+        Mockito.doThrow(new InvariantError("Validation failed")).when(validationUtil).validate(request);
+
+        InvariantError exception = Assertions.assertThrows(InvariantError.class, () -> authService.userLogin(request));
+
+        Assertions.assertEquals("Validation failed", exception.getMessage());
+    }
+
+    @Test
+    void userLogin_ShouldThrowAuthenticationError_WhenInvalidEmailCredentials() {
+        UserLoginRequest request = new UserLoginRequest(
+                "test@gmail.com",
+                "password"
+        );
+
+        Mockito.when(userRepository.findByEmail(request.getEmail())).thenReturn(Optional.empty());
+
+        AuthenticationError exception = Assertions.assertThrows(AuthenticationError.class, () -> authService.userLogin(request));
+
+        Assertions.assertEquals("Invalid credentials", exception.getMessage());
+    }
+
+    @Test
+    void userLogin_ShouldThrowAuthenticationError_WhenInvalidPasswordCredentials() {
+        UserLoginRequest request = new UserLoginRequest(
+                "test@gmail.com",
+                "password"
+        );
+
+        User existingUser = User.builder()
+                .email("test@example.com")
+                .password("hashedPassword")
+                .firstName("John")
+                .lastName("Doe")
+                .phoneNumber("123456789")
+                .build();
+
+        Mockito.when(userRepository.findByEmail(request.getEmail())).thenReturn(Optional.of(existingUser));
+
+        Mockito.when(passwordEncoder.matches(request.getPassword(), existingUser.getPassword())).thenReturn(false);
+
+        AuthenticationError exception = Assertions.assertThrows(AuthenticationError.class, () -> authService.userLogin(request));
+
+        Assertions.assertEquals("Invalid credentials", exception.getMessage());
+    }
+
+    @Test
+    void userLogin_ShouldThrowAuthenticationError_WhenUserNotEnable() {
+        UserLoginRequest request = new UserLoginRequest(
+                "test@gmail.com",
+                "password"
+        );
+
+        User existingUser = User.builder()
+                .email("test@example.com")
+                .password("hashedPassword")
+                .firstName("John")
+                .lastName("Doe")
+                .isEnable(false)
+                .phoneNumber("123456789")
+                .build();
+
+        Mockito.when(userRepository.findByEmail(request.getEmail())).thenReturn(Optional.of(existingUser));
+
+        Mockito.when(passwordEncoder.matches(request.getPassword(), existingUser.getPassword())).thenReturn(true);
+
+        AuthenticationError exception = Assertions.assertThrows(AuthenticationError.class, () -> authService.userLogin(request));
+
+        Assertions.assertEquals("Please activate your account", exception.getMessage());
+    }
+
+    @Test
+    void userLogin_ShouldLogin_WhenRequestIsValid() {
+        UserLoginRequest request = new UserLoginRequest(
+                "test@gmail.com",
+                "password"
+        );
+
+        User existingUser = User.builder()
+                .email("test@example.com")
+                .password("hashedPassword")
+                .firstName("John")
+                .lastName("Doe")
+                .isEnable(true)
+                .phoneNumber("123456789")
+                .build();
+
+        Mockito.when(userRepository.findByEmail(request.getEmail())).thenReturn(Optional.of(existingUser));
+
+        Mockito.when(passwordEncoder.matches(request.getPassword(), existingUser.getPassword())).thenReturn(true);
+
+        Mockito.when(jwtService.generateAccessToken(existingUser.getUsername())).thenReturn("accessToken");
+
+        Mockito.when(jwtService.generateRefreshToken(existingUser.getUsername())).thenReturn("refreshToken");
+
+        Authentication authentication = Authentication.builder()
+                .refreshToken("refreshToken")
+                .build();
+
+        Mockito.when(authenticationRepository.save(authentication)).thenReturn(authentication);
+
+        UserTokenResponse result = authService.userLogin(request);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("accessToken", result.getAccessToken());
+        Assertions.assertEquals("refreshToken", result.getRefreshToken());
     }
 }
