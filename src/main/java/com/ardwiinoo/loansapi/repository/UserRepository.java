5ca6365e023be637388d7e90bcb4845f123aa396
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-07 20:04:59 +0700

    add repo annotation

--- src/main/java/com/ardwiinoo/loansapi/repository/UserRepository.java
+++ src/main/java/com/ardwiinoo/loansapi/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.ardwiinoo.loansapi.repository;
 
 import com.ardwiinoo.loansapi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
