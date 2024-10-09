c797fe973c6c2fe82b8c7da853f44d8199ba2b9c
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-09 19:37:10 +0700

    add find by refresh token

--- src/main/java/com/ardwiinoo/loansapi/repository/AuthenticationRepository.java
+++ src/main/java/com/ardwiinoo/loansapi/repository/AuthenticationRepository.java
@@ -4,6 +4,10 @@ import com.ardwiinoo.loansapi.model.entity.Authentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthenticationRepository extends JpaRepository<Authentication, Integer> {
+
+    Optional<Authentication> findByRefreshToken(String refreshToken);
 }
