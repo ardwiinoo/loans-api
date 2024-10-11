69bab6d5898d90d3ebbed946b7f893aa294a1afd
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-11 10:57:07 +0700

    add loan add request

---
+++ src/main/java/com/ardwiinoo/loansapi/model/dto/loan/LoanAddRequest.java
@@ -0,0 +1,27 @@
+package com.ardwiinoo.loansapi.model.dto.loan;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class LoanAddRequest {
+
+    @NotNull
+    @Min(100000)
+    private BigDecimal amount;
+
+    @NotNull
+    private BigDecimal interestRate;
+
+    @NotNull
+    private LocalDateTime requestDate;
+
+    @NotNull
+    private LocalDateTime dueDate;
+}
