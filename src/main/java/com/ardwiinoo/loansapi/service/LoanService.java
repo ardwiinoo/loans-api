92be0ece204fa51530d6157f6d55271650ee00c2
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-14 08:25:48 +0700

    add detail loan, get all loans, and approve loan contract

--- src/main/java/com/ardwiinoo/loansapi/service/LoanService.java
+++ src/main/java/com/ardwiinoo/loansapi/service/LoanService.java
@@ -4,6 +4,11 @@ import com.ardwiinoo.loansapi.model.dto.loan.LoanAddRequest;
 import com.ardwiinoo.loansapi.model.dto.loan.LoanDto;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface LoanService {
     LoanDto addLoan(LoanAddRequest request, MultipartFile[] documents);
+    LoanDto getLoanById(Long id);
+    List<LoanDto> getAllLoans();
+    LoanDto approveLoan(Long id);
 }
