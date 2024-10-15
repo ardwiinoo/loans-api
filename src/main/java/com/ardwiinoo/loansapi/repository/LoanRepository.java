c764308ce5cc009e33114bad98830371a2924573
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-15 08:59:15 +0700

    add custom query findByUserId

--- src/main/java/com/ardwiinoo/loansapi/repository/LoanRepository.java
+++ src/main/java/com/ardwiinoo/loansapi/repository/LoanRepository.java
@@ -4,6 +4,10 @@ import com.ardwiinoo.loansapi.model.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    Optional<Loan> findByUserId(long userId);
 }
