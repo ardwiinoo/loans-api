477ec9547cc2bf54dc1a5402a1dbdfdd043225ef
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-29 21:45:24 +0700

    add lombok annotation

--- src/main/java/com/ardwiinoo/loansapi/model/dto/user/UserRefreshTokenRequest.java
+++ src/main/java/com/ardwiinoo/loansapi/model/dto/user/UserRefreshTokenRequest.java
@@ -1,9 +1,15 @@
 package com.ardwiinoo.loansapi.model.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserRefreshTokenRequest {
     @NotBlank
     private String refreshToken;
