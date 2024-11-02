b1805b3b5e82ff1e2ce23f22da4b8a455d726887
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-11-02 09:36:03 +0700

    add lombok annotation

--- src/main/java/com/ardwiinoo/loansapi/model/dto/user/UserVerifyRequest.java
+++ src/main/java/com/ardwiinoo/loansapi/model/dto/user/UserVerifyRequest.java
@@ -2,11 +2,13 @@ package com.ardwiinoo.loansapi.model.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class UserVerifyRequest {
     @Email
     @NotBlank
